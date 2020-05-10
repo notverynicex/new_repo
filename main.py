158331c52cc91418dc73afcd057853ef6103fede
notverynicex <xnicenonikx@gmail.com>
2020-05-10 19:05:31 +0300

    Dev2. Fixed string issue.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 print('Insert two numbers please (only positives)')
 while True:
-    a = int(input())
-    b = int(input())
-    if (a>-1) and (b>-1):
-        print(f"Thank you for inputting the numbers {a} and {b}")
-        print(f"The sum of the numbers is {a + b}")
-        print(f"The difference of numbers is {a - b}")
-        print(f"The result of number multiplication is {a * b}")
-        print(f"The result of number division is {a / b}")
-        break
-    elif (a<0) or (b<0) or (b==0):
-        print('You made a mistake, please verify the numbers!')
+    try:
+        a = int(input())
+        b = int(input())
+        if (a>-1) and (b>-1):
+            print(f"Thank you for inputting the numbers {a} and {b}")
+            print(f"The sum of the numbers is {a + b}")
+            print(f"The difference of numbers is {a - b}")
+            print(f"The result of number multiplication is {a * b}")
+            try:
+                print(f"The result of number division is {a / b}")
+            except ZeroDivisionError:
+                print("Cannot divide by zero.")
+            break
+        elif (a<0) or (b<0):
+            print('You made a mistake, please verify the numbers!')
+    except ValueError:
+        print("Nothing can be displayed here.")
